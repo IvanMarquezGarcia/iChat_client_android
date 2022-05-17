df801f3cbb20b5967a9b6f72361920b292558db5
Iván Márquez García <ivanmarquezgarcia00@gmail.com>
2022-05-18 00:30:39 +0200

    feat: send messages

--- app/src/main/java/com/ivan/iChat/ichat_android/ChatActivity.java
+++ app/src/main/java/com/ivan/iChat/ichat_android/ChatActivity.java
@@ -6,6 +6,8 @@ import androidx.recyclerview.widget.RecyclerView;
 
 import android.os.Bundle;
 
+import android.view.View;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.io.DataOutputStream;
@@ -25,8 +27,10 @@ public class ChatActivity extends AppCompatActivity {
 
     private int connectionStatus;
     private Socket socket;
+    private DataOutputStream output;
     private String username;
     private TextView error_text;
+    private EditText input;
     private RecyclerView mssgsList;
     private Thread readerThread;
 
@@ -36,11 +40,18 @@ public class ChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_chat);
 
         // initialize components
-        ((TextView) findViewById(R.id.chat_username_textview)).setText(getIntent().getStringExtra("username"));
+        username = getIntent().getStringExtra("username");
+        ((TextView) findViewById(R.id.chat_username_textview)).setText(username);
         socket = SingletonSocket.getInstance();
+        try {
+            output = new DataOutputStream(socket.getOutputStream());
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
         connectionStatus = CONNECTED;
-        mssgsList = findViewById(R.id.chat_mssgs_recyclerview);
         error_text = findViewById(R.id.chat_error_textview);
+        input = findViewById(R.id.chat_input_edittext);
+        mssgsList = findViewById(R.id.chat_mssgs_recyclerview);
         readerThread = new Thread(new ReaderRunnable(this));
     }
 
@@ -80,4 +91,39 @@ public class ChatActivity extends AppCompatActivity {
             System.out.println("La escucha ya está habilitada");
     }
 
+    public void send(View view) {
+        error_text.setVisibility(View.INVISIBLE);
+        if (connectionStatus == CONNECTED) {
+            String mssg = input.getText().toString().trim();
+
+            if (mssg.length() > 0) {
+                // send mssg to server
+                if (output != null) {
+                    new Thread(() -> {
+                        try {
+                            output.writeUTF("[" + username + "] > " + mssg + "");
+                            output.flush(); // Clear output stream
+                        } catch(IOException ioe) {
+                            String errorMsg = "Error al enviar mensaje";
+
+                            System.out.println("---------------------------------------------------------------------");
+                            //ioe.printStackTrace();
+                            System.out.println(errorMsg);
+                            System.out.println("---------------------------------------------------------------------");
+
+                            error_text.setText(errorMsg);
+                            error_text.setVisibility(View.VISIBLE);
+                        }
+                    }).start();
+
+                    input.setText(""); // Clear EditText text input
+                }
+                else {
+                    System.out.println("Flujo de salida nulo");
+                }
+                /*mensajes_ListView.getItems().add(new Mensaje(mensaje + " < [Tú]", true));
+                mensajes_ListView.scrollTo(mensajes_ListView.getItems().size() - 1);*/
+            }
+        }
+    }
 }
